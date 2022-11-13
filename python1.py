777dd9cbfe33795383a0f66e1a50f1153c1b67d7
WacperK <104558386+WacperK@users.noreply.github.com>
2022-11-13 16:12:38 +0100

    Simple control algorithm without P regulator
    Pierwsza wersja jezdzacego robota bez uzycia regulatora P. Zawiera tylko jedna wartosc korekcji orientacji wzdluz linii

--- python1.py
+++ python1.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_C, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_4, INPUT_1
+from ev3dev2.sensor import INPUT_4, INPUT_3
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.led import Leds
 
 # TODO: Add code here
 
 #wheels = MoveTank(OUTPUT_C, OUTPUT_A)
-
-SENSOR_MIDDLE = 17 	#wartość odbicia 50/50 na czarnej linii
-SENSOR_LEFT = 8 	#czujnik skrecajacy w linie
-SENSOR_RIGHT = 30	#czujnik poza linią
-
 
 try:
 	print("Definicja stalych")
@@ -25,23 +20,28 @@ try:
 
 	class Senses:
 		def __init__(self):
-			self.leftSensor = ColorSensor(INPUT_4)
-			self.rightSensor = ColorSensor(INPUT_1)
+			self.leftSensor = ColorSensor(INPUT_3)
+			self.rightSensor = ColorSensor(INPUT_4)
 			self.rightReadout = 0
 			self.leftReadout = 0
 			self.lastRightReadout = 0
 			self.lastLeftReadout = 0
 			self.alertValue = 28
+			self.lNormalValue = 35
+			self.rNormalValue = 28
+			self.errorTrigger = 8
+			self.leftAlert = self.lNormalValue - self.errorTrigger
+			self.rightAlert =  self.rNormalValue - self.errorTrigger
 
 		def readR(self):
 			temp = self.rightSensor.reflected_light_intensity
-			self.lastRightReadout = rightReadout
+			self.lastRightReadout = self.rightReadout
 			self.rightReadout = temp
 			return temp
 
 		def readL(self):
 			temp = self.leftSensor.reflected_light_intensity
-			self.lastLeftReadout = leftReadout
+			self.lastLeftReadout = self.leftReadout
 			self.leftReadout = temp
 			return temp
 
@@ -56,16 +56,16 @@ try:
 			self.readR()
 
 		def leftIsCrossing(self):
-			if lVal() <= self.alertValue:
-				return True:
+			if self.lVal() <= self.leftAlert:
+				return True
 			else:
-				return False:
+				return False
 
 		def rightIsCrossing(self):
-			if rVal() <= self.alertValue:
-				return True:
+			if self.rVal() <= self.rightAlert:
+				return True
 			else:
-				return False:
+				return False
 		'''
 		def isCrossing(self):
 			parameters = {}
@@ -84,8 +84,10 @@ try:
 				self.polarity = 'normal'
 			else:
 				self.polarity = 'inversed'
-			self.leftMotor = LargeMotor(OUTPUT_A, polarity=self.polarity)
-			self.rightMotor = LargeMotor(OUTPUT_C, polarity=self.polarity)
+			self.leftMotor = LargeMotor(OUTPUT_A)
+			self.rightMotor = LargeMotor(OUTPUT_C)
+			self.leftMotor.polarity=self.polarity
+			self.rightMotor.polarity=self.polarity
 			#Wartosci dodawane do silnika
 			self.rightAdd = 0
 			self.leftAdd = 0
@@ -94,21 +96,22 @@ try:
 
 		def setWheels(self, speedLeft, speedRight):
 			if speedLeft>100 or speedRight>100 or speedLeft<-100 or speedRight<-100:
-				print(f"Zla wartosc na silniku. L{speedLeft}; R{speedRight}")
+				print("Zla wartosc na silniku.")
+				print(speedLeft, speedRight)
 				return
 
 			self.leftMotor.on(SpeedPercent(speedLeft))
-			self.rightMotor.on(SpeedPercent(speedLeft))
+			self.rightMotor.on(SpeedPercent(speedRight))
 
 		
 		#Docelowa funkcja sterujaca(predkosc normalna+przyrosty)
 		def drive(self):
-			setWheels(self.normalSpeed + self.leftAdd, self.normalSpeed + self.rightAdd)
+			self.setWheels(self.normalSpeed + self.leftAdd, self.normalSpeed + self.rightAdd)
 
 		#Czy skreca?
 		def isTurning(self):
 			if self.rightAdd==0 and self.leftAdd==0:
-				return False:
+				return False
 			else:
 				return True
 
@@ -117,32 +120,62 @@ try:
 			if value > self.maxTurnAdd:
 				return
 			else:
-				leftAdd = value
+				self.leftAdd = value
 
-		def changeLAdd(self, value):
+		def changeRAdd(self, value):
 			if value > self.maxTurnAdd:
 				return
 			else:
-				leftAdd = value
+				self.rightAdd = value
+		
+		def turnRight(self):
+			self.rightAdd = -10
+			self.leftAdd = 5
+
+		def turnLeft(self):
+			self.leftAdd = -10
+			self.rightAdd = 5
+
+		def clearAdd(self):
+			self.rightAdd = 0
+			self.leftAdd = 0
+
 
 
 	print("Inicjalizacja sensorow oraz silnikow")
-	motors = Wheels(False)
+	motors = Wheels(True)
 	sense = Senses()
 
 	print('Entering the loop...')
 	#Glowna petla
 	while(1):
 		sense.readout()
-		motors.drive()
+		if sense.rightIsCrossing() or sense.leftIsCrossing():
+			if sense.rightIsCrossing() and sense.leftIsCrossing():
+				print('Skrzyzowanie')
+				motors.clearAdd()
+			elif sense.leftIsCrossing():
+				print('Skrecam w lewo')
+				motors.turnRight()
+			elif sense.rightIsCrossing():
+				print('Skrecam w prawo')
+				motors.turnLeft()
 
+		else:
+			motors.clearAdd
+			print('Prosto')
+
+
+		print(sense.lVal(), sense.rVal())	
+		motors.drive()
+		
 except KeyboardInterrupt: #ctrl+c
 	try:
 		motors.setWheels(0,0)
 	except:
 		pass
 
-	print("Program zakończony przez użytkownika")
+	print("Program zakonczony przez uzytkownika")
 			
 			
 		
