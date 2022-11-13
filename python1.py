73ec567ad8bf6172d38fc81506bf3e519925503c
WacperK <104558386+WacperK@users.noreply.github.com>
2022-11-13 16:15:25 +0100

    Error handling class
    Dodano klase odpowiedzialna za obliczanie korekcji dla poszczegolnych silnikow w zaleznosci od odchylenia odczytywanych wartoscin z czujnikow (regulator P)

--- python1.py
+++ python1.py
@@ -3,20 +3,12 @@ from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_4, INPUT_3
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.led import Leds
+from time import sleep
 
 # TODO: Add code here
 
-#wheels = MoveTank(OUTPUT_C, OUTPUT_A)
 
 try:
-	print("Definicja stalych")
-	#wheels.on_for_rotations(SpeedPercent(10), SpeedPercent(10), 10
-
-
-
-	print("Przypisywanie czujnikow")
-	# !!!!!!!!!!!ZMIEN PRZYPISANIE CZUJNIKOW ( W MODULACH TEZ ) !!!!!!!!!!!!!!
-
 
 	class Senses:
 		def __init__(self):
@@ -29,9 +21,11 @@ try:
 			self.alertValue = 28
 			self.lNormalValue = 35
 			self.rNormalValue = 28
-			self.errorTrigger = 8
-			self.leftAlert = self.lNormalValue - self.errorTrigger
-			self.rightAlert =  self.rNormalValue - self.errorTrigger
+			self.errorTrigger = 15
+			#self.leftAlert = self.lNormalValue - self.errorTrigger
+			#self.rightAlert =  self.rNormalValue - self.errorTrigger
+			self.leftAlert = 15
+			self.rightAlert = 15
 
 		def readR(self):
 			temp = self.rightSensor.reflected_light_intensity
@@ -66,6 +60,14 @@ try:
 				return True
 			else:
 				return False
+
+		def lostLine(self):
+			if self.rVal() > 30 and self.lVal() > 35:
+				return True
+			else:
+				return False
+
+		
 		'''
 		def isCrossing(self):
 			parameters = {}
@@ -73,12 +75,87 @@ try:
 			parameters['right'] = rightIsCrossing()
 			return parameters 
 		'''
+	
+
+	class ErrorHandler:
+		def __init__(self, sense):
+			self.sense = sense
+			self.lError = 0
+			self.rError = 0
+			self.lastLError = 0
+			self.lastRError = 0
+			#Parametry regulatora
+			self.proportional = 0.3
+			self.differential = 0
+			#Korekcje
+			self.rCorrection = 0
+			self.lCorrection = 0
+			self.lIntegral = 0
+			self.rIntegral = 0
+			#Korekcje skumulowane
+			
+
+			
+		def calcRCorrection(self):
+			self.rCorrection = self.rError * self.proportional
 
 
+		def calcLCorrection(self):
+			self.lCorrection = self.lError * self.proportional
+
+		def incLIntegral(self):
+			self.lIntegral+=1
+
+		def incRIntegral(self):
+			self.rIntegral+=1
+
+		def clearRIntegral(self):
+			self.rIntegral = 0
+
+		def clearLIntegral(self):
+			self.lIntegral = 0
+
+		def updateValues(self):
+			self.lastLError = self.lError
+			self.lastRError = self.rError
+			if sense.lVal() <= sense.leftAlert:
+				self.lError = sense.leftAlert - sense.lVal()
+			else:
+				self.lError = 0
+
+			if sense.rVal() <= sense.rightAlert:
+				self.rError = sense.rightAlert - sense.rVal()
+			else:
+				self.rError = 0
+			#self.lError = self.sense.lNormalValue -  self.sense.leftReadout
+			#self.rError = self.sense.rNormalValue - self.sense.rightReadout	
+			self.calcRCorrection()
+			self.calcLCorrection()
+
+		def returnLIntegral():
+			if self.lIntegral <= 20:
+				return int(self.lIntegral)
+			else:
+				return 20
+
+		def returnRIntegral():
+			if self.rIntegral <= 20:
+				return int(self.rIntegral)
+			else:
+				return 20
+		
+		def returnLCorr(self):
+			return int(self.lCorrection)
+
+		def returnRCorr(self):
+			return int(self.rCorrection)
+			
+			
+			
 
 	print("Definicja klasy kolek")
 	class Wheels:
-		def __init__(self, reverseWheels=False):
+		def __init__(self, reverseWheels=False, errorHandler=None):
 			#obrocenie dzialania silnikow
 			if reverseWheels==False:
 				self.polarity = 'normal'
@@ -92,13 +169,19 @@ try:
 			self.rightAdd = 0
 			self.leftAdd = 0
 			self.maxTurnAdd = 10
-			self.normalSpeed = 20
+			self.normalSpeed = 12
+			self.lastTurn = '0'
+			#ErrorHandler
+			self.error = errorHandler
 
 		def setWheels(self, speedLeft, speedRight):
+			#check wartosci, czy nie przekroczono wartosci maksymalnych
 			if speedLeft>100 or speedRight>100 or speedLeft<-100 or speedRight<-100:
 				print("Zla wartosc na silniku.")
 				print(speedLeft, speedRight)
 				return
+
+			#odpalenie silnikow
 
 			self.leftMotor.on(SpeedPercent(speedLeft))
 			self.rightMotor.on(SpeedPercent(speedRight))
@@ -115,6 +198,19 @@ try:
 			else:
 				return True
 
+		def isTurningRight(self):
+			if self.leftAdd > 0 or self.error.lIntegral > 0:
+				return True
+			else:
+				return False
+
+		def isTurningLeft(self):
+			if self.rightAdd > 0 or self.error.rIntegral > 0:
+				return True
+			else:
+				return False
+
+
 		#zmiana wartosci delty predkosci
 		def changeLAdd(self, value):
 			if value > self.maxTurnAdd:
@@ -129,45 +225,82 @@ try:
 				self.rightAdd = value
 		
 		def turnRight(self):
-			self.rightAdd = -10
-			self.leftAdd = 5
+			self.rightAdd = self.error.returnRCorr()
+			self.leftAdd = -20
+			self.lastTurn = 'r'
 
 		def turnLeft(self):
-			self.leftAdd = -10
-			self.rightAdd = 5
+			self.leftAdd = self.error.returnLCorr()
+			self.rightAdd = -20
+			self.lastTurn = 'l' 
 
 		def clearAdd(self):
 			self.rightAdd = 0
-			self.leftAdd = 0
+			self.leftAdd = 0 
+
+		def setNormalSpeed(self, speed):
+			self.normalSpeed = speed
+
+		def checkLine(self):
+			if self.error.sense.lostLine():
+				self.setNormalSpeed(0)
+
+			else:
+			 	self.setNormalSpeed(20)
+			
 
 
 
 	print("Inicjalizacja sensorow oraz silnikow")
-	motors = Wheels(True)
 	sense = Senses()
+	errHandler  = ErrorHandler(sense)
+	motors = Wheels(True, errHandler)
+	
 
 	print('Entering the loop...')
 	#Glowna petla
 	while(1):
+
 		sense.readout()
-		if sense.rightIsCrossing() or sense.leftIsCrossing():
+		errHandler.updateValues()
+		
+		if sense.rightIsCrossing() or sense.leftIsCrossing() or sense.lostLine():
 			if sense.rightIsCrossing() and sense.leftIsCrossing():
 				print('Skrzyzowanie')
 				motors.clearAdd()
 			elif sense.leftIsCrossing():
 				print('Skrecam w lewo')
-				motors.turnRight()
+				motors.checkLine()
+				motors.turnLeft()
 			elif sense.rightIsCrossing():
 				print('Skrecam w prawo')
+				motors.checkLine()
+				motors.turnRight()
+			elif sense.lostLine():
+				if motors.lastTurn == 'r':
+					motors.turnRight()
+				elif motors.lastTurn == 'l':
+					motors.turnLeft()
+				else:
+					pass
+
+		elif(sense.lostLine()):
+			print("Linia Zgubiona")
+			if motors.lastTurn =='r':
+				motors.turnRight()
+			elif motors.lastTurn == 'l':
 				motors.turnLeft()
-
 		else:
-			motors.clearAdd
+			motors.clearAdd()
+			errHandler.clearRIntegral()
+			errHandler.clearLIntegral()
 			print('Prosto')
-
-
-		print(sense.lVal(), sense.rVal())	
+		
 		motors.drive()
+		
+		#print(sense.lVal(), sense.rVal())		
+		print(sense.lVal(), sense.rVal())	
+		
 		
 except KeyboardInterrupt: #ctrl+c
 	try:
